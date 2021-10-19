ed9df6aeda53b79e9bed1ac9863e062d84f2819d
jcstange <jcstange@gmail.com>
2021-10-19 15:07:37 +0300

    Adding parseInt to error handling

--- src/routes/channels.router.ts
+++ src/routes/channels.router.ts
@@ -18,10 +18,10 @@ channelsRouter.get('/', async(_req: Request, res: Response) => {
 channelsRouter.get('/:channelId', async(req: Request, res: Response) => {
     const id = req?.params?.channelId
     try {
-        if(id === 2) {
+        if(parseInt(id) === 2) {
             res.status(200).send(articles.channel)
         } else {
-            res.status(404).send("Information not available")
+            res.status(404).send(Error("Information not available"))
         }
     } catch(error) {
         res.status(404).send(error)
@@ -32,10 +32,10 @@ channelsRouter.get('/:channelId/:articleId', async(req: Request, res: Response) 
     const channelId = req?.params?.channelId
     const articleId = req?.params?.articleId
     try {
-        if(channelId === 2 && articleId === 1) {
+        if(parseInt(channelId) === 2 && parseInt(articleId) === 1) {
             res.status(200).send(article.article)
         } else {
-            res.status(404).send("Information not available")
+            res.status(404).send(Error("Information not available"))
         }
     } catch(error) {
         res.status(404).send(error)
